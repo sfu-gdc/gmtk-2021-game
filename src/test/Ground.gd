fe25890d19729ac4ce07ad1b2200d8e3caef8036
0Strategist0 <0strategist0@gmail.com>
2021-06-05 16:42:40 -0700

    Made some edits

--- src/test/Ground.gd
+++ src/test/Ground.gd
@@ -5,7 +5,3 @@ onready var sprite_manager_ref = get_node(NodePath("/root/Sample/SpriteManager")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.set_texture(sprite_manager_ref.get("ground_tex"))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
