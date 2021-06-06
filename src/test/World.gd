4339e1678bdcabe538d699a27074b0852afe1fe8
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-06-05 20:43:03 -0700

    updated tileset & fixed small bug

--- src/test/World.gd
+++ src/test/World.gd
@@ -13,8 +13,9 @@ onready var ground_choices = [
 	sprite_manager_ref.get_tex(sprite_manager_ref.get("ground3_rect"))
 ]
 
-# TODO: build textures here & place them down.
+# This class builds the world
 func _ready():
+	rand.seed = 99
 	background_ref.position = Vector2(-48, -24)
 	for y in range(0, 4):
 		for x in range(0, 7):
@@ -28,7 +29,7 @@ func _ready():
 	ground_ref.position = Vector2(-32, 8)
 	for x in range(0, 9):
 		var ground_node = Sprite.new()
-		ground_node.set_texture(ground_choices[rand.randi_range(0, 2)])
+		ground_node.set_texture(ground_choices[rand.randi_range(0, 3)])
 		ground_node.set_position(Vector2(x*8, 0))
 		ground_node.flip_h = rand.randi_range(0, 1) == 0
 		ground_ref.add_child(ground_node)
