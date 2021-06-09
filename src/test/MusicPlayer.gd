e9e17c4ab22e549d8d83062f2153d44881b7285f
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-06-08 22:31:46 -0700

    Implemented animation, moving, and static typing

--- src/test/MusicPlayer.gd
+++ src/test/MusicPlayer.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	self.play()
+	#self.play()
+	pass
--- src/test/World.gd
+++ src/test/World.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-onready var sprite_manager_ref = get_node(NodePath("/root/Sample/SpriteManager"))
-onready var background_ref = find_node("Background")
+onready var sprite_manager_ref: Node = get_node(NodePath("/root/Sample/SpriteManager"))
+onready var background_ref: Sprite = find_node("Background")
 
 # TODO: build textures here & place them down.
 func _ready():
-	background_ref.set_texture(sprite_manager_ref.get("background_tex"))
+	var tex: Texture = sprite_manager_ref.get_tex(sprite_manager_ref.background_rect)
+	background_ref.set_texture(tex)
