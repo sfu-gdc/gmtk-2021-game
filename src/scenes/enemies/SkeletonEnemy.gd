36fd69230376da4a7b4e0c5067e18536d7efdbbf
Richard <62489312+Richard172@users.noreply.github.com>
2021-06-11 17:38:39 -0700

    update enemy
    The enemy can now dashes at the player. It's affected by gravity and it can only dash left and right

--- src/scenes/enemies/SkeletonEnemy.gd
+++ src/scenes/enemies/SkeletonEnemy.gd
@@ -1,24 +1,61 @@
 extends KinematicBody2D
 
-var is_player_here: = false
-var velocity: = Vector2(0, 0)
-var acceleration: = 100
+var velocity: = Vector2.ZERO
+export var dash_speed: = 160
+export var chase_speed: = 80
+var gravity: = 1000
 var player = null
+var time = 0.0
+
+enum {
+	PREPARE,
+	DASH,
+	CHASE,
+	STOP
+}
+
+var state: = STOP
 
 
 func _physics_process(delta):
-	if is_player_here:
-		move_and_slide(velocity)
-		velocity = (player.global_position - global_position).normalized() * 100
+	match state:
+		CHASE:
+			chase(delta)
+		DASH:
+			dash(delta)
+		STOP:
+			stop(delta)
 
-func _on_Hurtbox_area_entered(area):
-	queue_free()
+
+func chase(delta):
+	velocity.x = chase_speed if player.global_position > global_position else -chase_speed
+	move(delta)
+	
+	
+func dash(delta):
+	yield(get_tree().create_timer(1.0), "timeout")
+	velocity.x = dash_speed if player.global_position > global_position else -dash_speed
+	move(delta)
+	
+
+func stop(delta):
+	velocity.x = 0.0
+	move(delta)
+
+
+func move(delta):
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity)
 
 
 func _on_Visionbox_area_entered(area):
 	player = area
-	is_player_here = true
+	state = CHASE
 
 
 func _on_Visionbox_area_exited(area):
-	is_player_here = false
+	state = STOP
+
+
+func _on_Dashbox_area_entered(area):
+	state = DASH
