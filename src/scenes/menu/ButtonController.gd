195f639e7025fbb03c61b030d06cdbc21b8a1811
Alex <metroid3d2@gmail.com>
2021-06-11 20:09:32 -0700

    Menu WIP

---
+++ src/scenes/menu/ButtonController.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	var menu_y := 2
+#	var menu_offset := 32
+#	var menu_index := 0
+#
+#	for button in self.get_children():
+#		menu_index += 1
+#		if button is Button:
+#			button.text = "Wheeee!"
+#			button.y = 0
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
